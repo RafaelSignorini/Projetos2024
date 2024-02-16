3e2acfab39b45eb167bf439a7a3a65e8e09c9524
RafaelSignorini <rafabs26126@gmail.com>
2024-02-16 14:09:52 -0300

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,9 @@
+AVAparcial1 = float(input("Nota da AVA parcial: "))
+AVAfinal1 = float(input("Nota da AVA final: "))
+AC1 = float(input("Nota da AC: "))
+Produtividade1 = float(input("Nota da produtividade: "))
+AVAp01 = AVAparcial1 * 0.25
+AVAf01 = AVAfinal1 * 0.25
+AC01 = 
 
-
-AVAparcial = float(input("Nota da AVA parcial: "))
-AVAfinal = float(input("Nota da AVA final: "))
-AC = float(input("Nota da AC: "))
-Produtividade = float(input("Nota da produtividade: "))
+M1 = AVAparcial1 + AVAfinal1 + AC1 + Produtividade1

701215c0d64cab9176a97dbe644ccab0d9d346e2
Senhor_Ini <rafabs26126@gmail.com>
2024-04-04 21:32:21 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -1,14 +1,12 @@
 import math 
 
 # Constantes
-k = 9 * 10 ** 9     # Constante de Coulomb (N m^2/C^2)
-d = 0.09            # Distância entre as cargas (m)
-F = 4 * 10 ** -3    # Força eletrostática (N)
+# Constante de Coulomb (N m^2/C^2)
+# Distância entre as cargas (m)
+# Força eletrostática (N)
 
 # Cálculo das cargas
-q1_q2 = F * d ** 2 / k
 
 # Impressão do resultado
-print('q1 * q2 = {} C^2'.format(q1_q2))
 
 # Supostamente o resultado era pra dar 6e-15 C^2

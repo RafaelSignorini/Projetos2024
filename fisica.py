bd2fc1aa9a3fc4ad47956ac072f5dc49f48c699e
Senhor_Ini <rafabs26126@gmail.com>
2024-04-04 22:58:19 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -8,4 +8,16 @@ def descubraForca():
     k = 9 * (10**9)                                                         
     d = 5 * (10**-2)                                                        
     return "A váriavel 'F' tem o valor de {}".format(k * abs(q1 * q2) / d**2)  
-print(descubraForca())
+
+def descubraCampoAfastado():
+    dQuadrado = 40**2 + 20**2
+    hipotenusa = dQuadrado**(1/2)
+
+    Qa = 48 * (10**-6)
+    Qb = 16 * (10**-6)
+    k = 9 * (10**9)
+    F = 40 * (10**5)
+    d = (k * abs(Qa * Qb / F)**0.5)
+    return "A variável 'F' tem o valor de {} e a distância 'd' é {:.2f}".format(F, d)
+
+print(descubraCampoAfastado())

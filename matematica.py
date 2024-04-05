a6e45088fcc40708b8e8fbe2f1ba200cdf3e6961
Senhor_Ini <rafabs26126@gmail.com>
2024-04-05 16:50:18 -0300

    Update matematica.py

--- matematica.py
+++ matematica.py
@@ -7,6 +7,7 @@ def jurosAnualComposto():
     return "O juros após {} anos sobre um capital de {} reais e uma taxa de {}%, será de {}".format(t, C, i * 100, M - C)
 
 def taxaMensalComposto():
+    # Este código não está perfeitamente funcional
     C = float(input("Digite o valor do capital: \n"))
     t = float(input("Digite o tempo em que será investido o dinheiro: \n"))
     M = float(input("Digite o valor montante após o investimento: \n"))

fd46594a1808585a60a4734adcf9924f487466ce
Senhor_Ini <rafabs26126@gmail.com>
2024-04-04 21:45:12 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -1,12 +1,14 @@
 import math 
 
 # Constantes
-# Constante de Coulomb (N m^2/C^2)
-# Distância entre as cargas (m)
-# Força eletrostática (N)
+# Fórmula: F = k * (q1 * q2) / d**2
+q1 = 3 * (10**-6)
+q2 = 5 * (10**-6)
+k = 9 * (10**9)                                                         # Constante de Coulomb (N m^2/C^2)
+d = 5 * (10**-2)                                                        # Distância entre as cargas (m)
+print("A váriavel 'F' tem o valor de {}".format(k * (q1 * q2) / d**2))  # Força eletrostática (N)
 
-# Cálculo das cargas
 
-# Impressão do resultado
+# Impressão do resulta
 
-# Supostamente o resultado era pra dar 6e-15 C^2
+# Supostamente o resultado era 

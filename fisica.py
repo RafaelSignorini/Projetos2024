e992592e31d53d3fd1ae2af10669f5a528f2fa33
Senhor_Ini <rafabs26126@gmail.com>
2024-04-04 21:56:45 -0300

    Update fisica.py

--- fisica.py
+++ fisica.py
@@ -1,9 +1,11 @@
-import math 
+def descubraForca():
+    import math 
 
-# Constantes
-# Fórmula: F = k * (q1 * q2) / d**2
-q1 = 3 * (10**-6)
-q2 = 5 * (10**-6)
-k = 9 * (10**9)                                                         # Constante de Coulomb (N m^2/C^2)
-d = 5 * (10**-2)                                                        # Distância entre as cargas (m)
-print("A váriavel 'F' tem o valor de {}".format(k * (q1 * q2) / d**2))  # Impressão do resultado
+    # Constantes
+    # Fórmula: F = k * (q1 * q2) / d**2
+    q1 = 3 * (10**-6)
+    q2 = 5 * (10**-6)
+    k = 9 * (10**9)                                                         
+    d = 5 * (10**-2)                                                        
+    return "A váriavel 'F' tem o valor de {}".format(k * abs(q1 * q2) / d**2)  
+print(descubraForca())
